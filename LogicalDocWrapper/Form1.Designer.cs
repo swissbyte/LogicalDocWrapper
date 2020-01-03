87118c3d9155313e4b9066ac33d4cc24bde5b33f
Claudio Hediger <claudio@databyte.ch>
2020-01-03 13:56:14 +0100

    First working code and first commit

--- LogicalDocWrapper/Form1.Designer.cs
+++ LogicalDocWrapper/Form1.Designer.cs
@@ -29,11 +29,350 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.virtConsole = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.tabPage4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(8, 176);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(498, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start LogicalDoc";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ServerPort:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(96, 24);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(8, 8);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(208, 112);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Tomcat/Catalina";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(8, 128);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(208, 80);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "MariaDB";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(96, 24);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ServerPort:";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(521, 254);
+            this.tabControl1.TabIndex = 5;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button3);
+            this.tabPage1.Controls.Add(this.panel2);
+            this.tabPage1.Controls.Add(this.panel1);
+            this.tabPage1.Controls.Add(this.label6);
+            this.tabPage1.Controls.Add(this.label5);
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(513, 225);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Home";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.groupBox3);
+            this.tabPage2.Controls.Add(this.groupBox1);
+            this.tabPage2.Controls.Add(this.groupBox2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(513, 225);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Settings";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 25);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(513, 225);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "About";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.textBox4);
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Controls.Add(this.textBox3);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Location = new System.Drawing.Point(224, 8);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(272, 112);
+            this.groupBox3.TabIndex = 5;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Paths";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(104, 24);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(140, 22);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            this.textBox3.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.textBox3_MouseDoubleClick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 17);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "LogicalDoc:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(104, 54);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(140, 22);
+            this.textBox4.TabIndex = 4;
+            this.textBox4.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.textBox4_MouseDoubleClick);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 56);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "MariaDB:";
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(8, 96);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(498, 40);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Kill LogicalDoc";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(16, 16);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 17);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "LogicalDoc:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(16, 48);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(66, 17);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "MariaDB:";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.IndianRed;
+            this.panel1.Location = new System.Drawing.Point(102, 18);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(16, 16);
+            this.panel1.TabIndex = 4;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.IndianRed;
+            this.panel2.Location = new System.Drawing.Point(102, 49);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(16, 16);
+            this.panel2.TabIndex = 5;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Controls.Add(this.virtConsole);
+            this.tabPage4.Location = new System.Drawing.Point(4, 25);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(513, 225);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Console";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // virtConsole
+            // 
+            this.virtConsole.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.virtConsole.Location = new System.Drawing.Point(3, 3);
+            this.virtConsole.Multiline = true;
+            this.virtConsole.Name = "virtConsole";
+            this.virtConsole.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.virtConsole.Size = new System.Drawing.Size(507, 219);
+            this.virtConsole.TabIndex = 0;
+            this.virtConsole.WordWrap = false;
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Location = new System.Drawing.Point(8, 136);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(498, 40);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Stop LogicalDoc";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(521, 254);
+            this.Controls.Add(this.tabControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "LogicalDocWrapper";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.tabPage4.ResumeLayout(false);
+            this.tabPage4.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.TextBox virtConsole;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- LogicalDocWrapper/Form1.cs
+++ LogicalDocWrapper/Form1.cs
@@ -7,14 +7,263 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
+using System.Collections;
+using System.Runtime.InteropServices;
 
 namespace LogicalDocWrapper
 {
     public partial class Form1 : Form
     {
+        private Process LogicalDocProc = new Process();
+        private Process MariaDBProc = new Process();
+
+        private const int SW_HIDE = 0;
+        private const int SW_SHOWNORMAL = 1;
+        private const int SW_SHOWMINIMIZED = 2;
+        private const int SW_SHOWMAXIMIZED = 3;
+
+        [DllImport("user32.dll")]
+        private static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void runLogicalDoc()
+        {
+            LogicalDocProc.StartInfo.UseShellExecute = false;
+            // You can start any process, HelloWorld is a do-nothing example.
+            LogicalDocProc.StartInfo.FileName = textBox3.Text + "\\bin\\catalina.bat";
+            LogicalDocProc.StartInfo.Arguments = "run";
+            LogicalDocProc.StartInfo.EnvironmentVariables["CATALINA_HOME"] = textBox3.Text;
+
+            LogicalDocProc.StartInfo.CreateNoWindow = true;
+            LogicalDocProc.StartInfo.RedirectStandardOutput = true;
+            LogicalDocProc.OutputDataReceived += new DataReceivedEventHandler(SortOutputHandler);
+
+
+            LogicalDocProc.Start();
+
+            LogicalDocProc.BeginOutputReadLine();
+        }
+
+        private void runMariaDB()
+        {
+            MariaDBProc.StartInfo.UseShellExecute = false;
+            // You can start any process, HelloWorld is a do-nothing example.
+            MariaDBProc.StartInfo.FileName = textBox4.Text + "\\bin\\mysqld";
+            MariaDBProc.StartInfo.Arguments = "--console --skip-grant-tables";
+
+            MariaDBProc.StartInfo.CreateNoWindow = false;
+            MariaDBProc.StartInfo.RedirectStandardOutput = true;
+            //MariaDBProc.StartInfo.RedirectStandardInput = true;
+            
+
+            MariaDBProc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            MariaDBProc.OutputDataReceived += new DataReceivedEventHandler(OutputHandler);
+            MariaDBProc.Start();
+
+            MariaDBProc.BeginOutputReadLine();
+
+            while (MariaDBProc.MainWindowHandle.Equals(IntPtr.Zero)) ;
+
+
+            IntPtr hWnd = MariaDBProc.MainWindowHandle;
+            if (!hWnd.Equals(IntPtr.Zero))
+            {
+                // SW_SHOWMAXIMIZED to maximize the window
+                // SW_SHOWMINIMIZED to minimize the window
+                // SW_SHOWNORMAL to make the window be normal size
+                ShowWindowAsync(hWnd, SW_HIDE);
+            }
+            
+
+    }
+
+        static void OutputHandler(object sendingProcess, DataReceivedEventArgs outLine)
+        {
+            //* Do your stuff with the output (write to console/log/StringBuilder)
+            Console.WriteLine(outLine.Data);
+        }
+
+        private void stopLogicalDoc()
+        {
+            Process shutdownProc = new Process();
+            shutdownProc.StartInfo.UseShellExecute = false;
+            // You can start any process, HelloWorld is a do-nothing example.
+            shutdownProc.StartInfo.FileName = textBox3.Text + "\\bin\\shutdown.bat";
+            shutdownProc.StartInfo.EnvironmentVariables["CATALINA_HOME"] = textBox3.Text;
+
+            shutdownProc.StartInfo.CreateNoWindow = true;
+            shutdownProc.StartInfo.RedirectStandardOutput = true;
+            //LogicalDocProc.OutputDataReceived += new DataReceivedEventHandler(SortOutputHandler);
+
+            shutdownProc.Start();
+            //LogicalDocProc.BeginOutputReadLine();
+            int i = 0;
+            while(!LogicalDocProc.HasExited)
+            {
+                System.Threading.Thread.Sleep(10);
+                i++;
+                if(i > 500)
+                {
+                    KillProcess(LogicalDocProc.Id);
+                }
+            }
+
+            //KillProcess(LogicalDocProc.Id);
+
+        }
+
+        private int startLogicalDoc()
+        {
+            Process[] processes = Process.GetProcesses();
+            List<int> oldPIDs = new List<int>();
+
+            foreach (Process process in processes)
+            {
+                if (process.ProcessName == "java")
+                {
+                    virtConsole.AppendText(process.ProcessName + " - " + process.Id.ToString() + "\r\n");
+                    oldPIDs.Add(process.Id);
+                }
+            }
+
+            runLogicalDoc();
+
+            LogicalDocProc = null;
+            while (LogicalDocProc == null)
+            {
+                processes = Process.GetProcesses();
+                foreach (Process process in processes)
+                {
+                    if (process.ProcessName == "java")
+                    {
+
+                        if (oldPIDs.Contains(process.Id) == false)
+                        {
+                            virtConsole.AppendText("LD Process found: " + process.ProcessName + " - " + process.Id.ToString() + "\r\n");
+                            LogicalDocProc = process;
+                        }
+                    }
+                }
+            }
+
+            return 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            runMariaDB();
+            startLogicalDoc();
+        }
+
+        void SortOutputHandler(object sender, DataReceivedEventArgs e)
+        {
+            Trace.WriteLine(e.Data);
+            this.BeginInvoke(new MethodInvoker(() =>
+            {
+                virtConsole.AppendText(e.Data ?? string.Empty + "\r\n");
+            }));
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        public void KillProcess(int pid)
+        {
+            Process[] process = Process.GetProcesses();
+
+            foreach (Process prs in process)
+            {
+                if (prs.Id == pid)
+                {
+                    prs.Kill();
+                    break;
+                }
+            }
+        }
+
+        private void textBox3_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            folderBrowserDialog1.SelectedPath = textBox3.Text;
+            folderBrowserDialog1.ShowDialog();
+            textBox3.Text = folderBrowserDialog1.SelectedPath;
+            
+            Properties.Settings.Default["LogicalDocPath"] = textBox3.Text;
+            Properties.Settings.Default.Save();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox3.Text = Properties.Settings.Default["LogicalDocPath"].ToString();
+            textBox4.Text = Properties.Settings.Default["MariaDBPath"].ToString();
+        }
+
+        private void textBox4_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            folderBrowserDialog1.SelectedPath = textBox4.Text;
+            folderBrowserDialog1.ShowDialog();
+            textBox4.Text = folderBrowserDialog1.SelectedPath;
+
+            Properties.Settings.Default["MariaDBPath"] = textBox4.Text;
+            Properties.Settings.Default.Save();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            LogicalDocProc.Kill();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //stopLogicalDoc();
+            MariaDBProc.CloseMainWindow();
+            MariaDBProc.Dispose();
+            MariaDBProc = new Process();
+
+            stopLogicalDoc();
+
+            LogicalDocProc.Dispose();
+            LogicalDocProc = new Process();
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!MariaDBProc.HasExited)  panel2.BackColor = Color.Green;
+                else panel2.BackColor = Color.Red;
+            }
+            catch
+            {
+                panel2.BackColor = Color.Red;
+            }
+
+            try
+            {
+                if (!LogicalDocProc.HasExited) panel1.BackColor = Color.Green;
+                else panel1.BackColor = Color.Red;
+            }
+            catch
+            {
+                panel1.BackColor = Color.Red;
+            }
         }
     }
 }
